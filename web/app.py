564c982d3a4c82176ca692ae4cca16fcfa682de3
Richo Healey <richo@psych0tik.net>
2018-03-09 16:06:56 -0800

    Closer to a functional webview for dropbox/youtube

--- web/app.py
+++ web/app.py
@@ -21,6 +21,9 @@ import google.oauth2.credentials
 DROPBOX_KEY = os.getenv('DROPBOX_KEY')
 DROPBOX_SECRET = os.getenv('DROPBOX_SECRET')
 
+YOUTUBE_KEY = os.getenv('YOUTUBE_KEY')
+YOUTUBE_SECRET = os.getenv('YOUTUBE_SECRET')
+
 app = Flask(__name__)
 app.secret_key = base64.b64encode(os.urandom(64))
 
@@ -31,30 +34,71 @@ def get_dropbox_flow():
     return dropbox.client.DropboxOAuth2Flow(DROPBOX_KEY, DROPBOX_SECRET,
             redirect_uri, session, "dropbox-auth-csrf-token")
 
+def get_youtube_flow():
+# TODO we have a key and secret
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            CLIENT_SECRETS_FILE, scopes=SCOPES)
+    flow.redirect_uri = flask.url_for('/youtube/finish', _external=True)
+    return flow
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/youtube/authorise')
-def authorize():
-    pass
+def youtube_authorize():
+    flow = get_youtube_flow()
+    authorization_url, state = flow.authorization_url(
+          # This parameter enables offline access which gives your application
+          # both an access and refresh token.
+          access_type='offline',
+          # This parameter enables incremental auth.
+          include_granted_scopes='true')
+
+    # Store the state in the session so that the callback can verify that
+    # the authorization server response.
+    flask.session['state'] = state
+
+    return redirect(authorization_url)
 
 @app.route('/youtube/finish')
-def finish():
-    pass
+def youtube_finish():
+    # Specify the state when creating the flow in the callback so that it can
+    # verify the authorization server response.
+    state = flask.session['state']
+# TODO this is not how this works
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = flask.url_for('/youtube/finish', _external=True)
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    credentials = flow.credentials
+    flask.session['youtube'] = {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes
+    }
+
+    return flask.redirect(flask.url_for('/'))
 
 
 @app.route('/dropbox/authorise')
-def authorize():
-    url = get_flow().start()
+def dropbox_authorize():
+    url = get_dropbox_flow().start()
     return redirect(url)
 
 
 @app.route('/dropbox/finish')
-def finish():
+def dropbox_finish():
     access_token, _, _ = get_flow().finish(request.args)
-    session["dropbox"]["access_token"]
+    session["dropbox"]["access_token"] = access_token
 
 
 @app.route('/config')
