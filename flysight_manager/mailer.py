26ebfa23518217bccbc233ae04e8ef0d3adbe54d
Richo Healey <richo@psych0tik.net>
2018-04-25 19:39:42 -0700

    Use newer sendgrid api

--- flysight_manager/mailer.py
+++ flysight_manager/mailer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import sendgrid
+from sendgrid.helpers.mail import *
 import log
 
 @log.make_loggable
 class Mailer(object):
     def __init__(self, cfg):
         self.token = cfg.token
-        self.client = sendgrid.SendGridClient(self.token)
+        self.sg = sendgrid.SendGridAPIClient(apikey=self.token)
 
     def mail(self, mail_to, mail_from, subject, body, headers={}):
         message = sendgrid.Mail()
@@ -17,3 +18,17 @@ class Mailer(object):
         message.set_html(body)
 
         self.client.send(message)
+
+        content = Content("text/html", body)
+        mail = Mail(Email(mail_from), subject, Email(mail_to), content)
+
+        return self.sg.client.mail.send.post(request_body=mail.get())
+
+
+@log.make_loggable
+class Formatter(object):
+    def __init__(self, template):
+        pass
+
+    def format(self, data):
+        pass
