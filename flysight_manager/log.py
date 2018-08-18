e7a70614c1127f7c27b7a21ce0094efec4cd72ab
Richo Healey <richo@psych0tik.net>
2018-08-17 23:39:24 -0700

    flake8

--- flysight_manager/log.py
+++ flysight_manager/log.py
@@ -3,11 +3,14 @@ import sys
 import functools
 import traceback
 
+
 class Log(object):
     OUTPUT = sys.stdout
 
+
 def suppress_logs():
     Log.OUTPUT = open(os.devnull, 'w')
+
 
 class LogAggregator(object):
     logs = None
@@ -38,24 +41,31 @@ def make_logger(c, enabled=lambda: True):
 def fatal(msg):
     make_logger("!")(msg)
     sys.exit(1)
+
+
 debug = make_logger("*", lambda: os.getenv("DEBUG"))
 info = make_logger("+")
 warn = make_logger("!")
 
+
 def make_loggable(cls):
     name = cls.__name__
+
     def discard_self(func):
         return lambda _, *args, **kwargs: func(*args, **kwargs)
 
     cls.debug = discard_self(make_logger("* %s" % name, lambda: os.getenv("DEBUG")))
     cls.info = discard_self(make_logger("+ %s" % name))
     cls.warn = discard_self(make_logger("! %s" % name))
+
     def fatal(msg):
         discard_self(make_logger("! %s" % name)(msg))
         sys.exit(1)
+
     cls.fatal = fatal
 
     return cls
+
 
 def catch_exceptions_and_retry(report):
     def outer(func):
