6014e12718924911ca6cde227afe19c9bd4903f4
Richo Healey <richo@psych0tik.net>
2018-08-17 23:34:57 -0700

    flake8

--- flysight_manager/notifier.py
+++ flysight_manager/notifier.py
@@ -1,17 +1,18 @@
 import requests
 
+
 class Notifier(object):
     pass
+
 
 class PushoverNotifier(Notifier):
     def __init__(self, token, user):
         self.token = token
         self.user = user
 
-
     def notify(self, message):
-        return requests.post("https://api.pushover.net/1/messages.json", data = {
+        return requests.post("https://api.pushover.net/1/messages.json", data={
             "token": self.token,
             "user": self.user,
             "message": message,
-            })
+        })
