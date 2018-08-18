e3dc366773673f2d5eefe5b026e38e15d660865e
Richo Healey <richo@psych0tik.net>
2018-08-18 09:18:59 -0700

    flake8

--- web/app.py
+++ web/app.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 import io
 import os
-import sys
 import toml
 import base64
 
+import flask
 from flask import Flask, render_template, redirect, request, url_for, \
-    session, Response, send_file
+    session, send_file
 import dropbox
 
 import google_auth_oauthlib.flow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaFileUpload
-from google_auth_oauthlib.flow import InstalledAppFlow
-from oauth2client.client import AccessTokenCredentials
-import oauth2client.client
-import google.oauth2.credentials
 
 DROPBOX_KEY = os.getenv('DROPBOX_KEY')
 DROPBOX_SECRET = os.getenv('DROPBOX_SECRET')
@@ -32,12 +25,13 @@ def get_dropbox_flow():
     redirect_uri = url_for('/dropbox/finish', _external=True, _scheme='https')
 
     return dropbox.client.DropboxOAuth2Flow(DROPBOX_KEY, DROPBOX_SECRET,
-            redirect_uri, session, "dropbox-auth-csrf-token")
+                                            redirect_uri, session, "dropbox-auth-csrf-token")
+
 
 def get_youtube_flow():
-# TODO we have a key and secret
+    # TODO we have a key and secret
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-            CLIENT_SECRETS_FILE, scopes=SCOPES)
+            CLIENT_SECRETS_FILE, scopes=SCOPES) # noqa: F821
     flow.redirect_uri = flask.url_for('/youtube/finish', _external=True)
     return flow
 
@@ -46,15 +40,16 @@ def get_youtube_flow():
 def index():
     return render_template('index.html')
 
+
 @app.route('/youtube/authorise')
 def youtube_authorize():
     flow = get_youtube_flow()
     authorization_url, state = flow.authorization_url(
-          # This parameter enables offline access which gives your application
-          # both an access and refresh token.
-          access_type='offline',
-          # This parameter enables incremental auth.
-          include_granted_scopes='true')
+        # This parameter enables offline access which gives your application
+        # both an access and refresh token.
+        access_type='offline',
+        # This parameter enables incremental auth.
+        include_granted_scopes='true')
 
     # Store the state in the session so that the callback can verify that
     # the authorization server response.
@@ -62,14 +57,15 @@ def youtube_authorize():
 
     return redirect(authorization_url)
 
+
 @app.route('/youtube/finish')
 def youtube_finish():
     # Specify the state when creating the flow in the callback so that it can
     # verify the authorization server response.
     state = flask.session['state']
-# TODO this is not how this works
+    # TODO this is not how this works
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state) # noqa: F821
     flow.redirect_uri = flask.url_for('/youtube/finish', _external=True)
 
     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
@@ -97,15 +93,16 @@ def dropbox_authorize():
 
 @app.route('/dropbox/finish')
 def dropbox_finish():
-    access_token, _, _ = get_flow().finish(request.args)
+    access_token, _, _ = get_dropbox_flow().finish(request.args)
     session["dropbox"]["access_token"] = access_token
 
 
 @app.route('/config')
 def config():
     return send_file(build_config(), mimetype="text/ini",
-            as_attachment=True,
-            attachment_filename="flysight-manager.ini")
+                     as_attachment=True,
+                     attachment_filename="flysight-manager.ini")
+
 
 def build_config():
     config = toml.load(open('flysight-manager.ini.example'))
@@ -116,7 +113,6 @@ def build_config():
 
     writer.seek(0)
     return writer
-
 
 
 if __name__ == "__main__":
