4f53109843ea087b8ee59376a1665aa86dd68d64
Richo Healey <richo@psych0tik.net>
2018-08-18 09:12:58 -0700

    flake8

--- setup.py
+++ setup.py
@@ -11,28 +11,30 @@ import unittest
 
 from flysight_manager.version import VERSION
 
+
 def test_suite():
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover('tests', pattern='test_*.py')
     return test_suite
 
+
 setup(
-        name='flysight_manager',
-        description="Tool for managing flysight data",
-        long_description=open('README.md').read(),
-        url="https://github.com/richo/flysight-manager",
-        version=VERSION,
-        author="Richo Healey",
-        author_email="richo@psych0tik.net",
-        license="MIT",
-        packages=[
-            'flysight_manager',
-            ],
-        entry_points={
-            'console_scripts': [
-                'flysight-manager = flysight_manager:main',
-                'gopro-manager = flysight_manager.gopro_manager:main',
-                ],
-            },
-        test_suite='setup.test_suite',
-        )
+    name='flysight_manager',
+    description="Tool for managing flysight data",
+    long_description=open('README.md').read(),
+    url="https://github.com/richo/flysight-manager",
+    version=VERSION,
+    author="Richo Healey",
+    author_email="richo@psych0tik.net",
+    license="MIT",
+    packages=[
+        'flysight_manager',
+    ],
+    entry_points={
+        'console_scripts': [
+            'flysight-manager = flysight_manager:main',
+            'gopro-manager = flysight_manager.gopro_manager:main',
+        ],
+    },
+    test_suite='setup.test_suite',
+)
